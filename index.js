c661dc418bae643ae347706890ebadb2584f2d58
noreencaalam <noreencaalam145@gmail.com>
2024-11-12 14:05:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,101 @@
 //instantiation
-//import express API framework
 const express = require("express")
-const app = express();
-//importing mysql
-const mysql = require("mysql")
-//port number
-const PORT = process.env.PORT || 5000;
+const app = express()
 
-//connection to mysql
-const connection = mysql.createConnection({
+const mysql =  require("mysql")
+const moment = require("moment")
+
+const PORT = process.env.PORT || 5001
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl} : ${moment().format()}`)
+    next();
+}
+
+app.use(logger);
+
+
+const connection =  mysql.createConnection({
     host: "brjcgzfsoddavnxdvgmg-mysql.services.clever-cloud.com",
     user: "ububnh7p6s5texzi",
     password: "JA75q7foksPb3y1tFA6M",
     database: "brjcgzfsoddavnxdvgmg",
 });
 
-//initilization of connection
 connection.connect();
 
+//REPORT - CRUD
+app.get("/api/members", (req, res) => {
 
-//API - REPORT
-//GET request and response are the parameters
-app.get("/api/members", (req, res) =>{
-    //create a query
-    connection.query("SELECT * FROM userdata",(err, rows, fields)=>{
-        //checking errors
+    connection.query("SELECT * FROM userdata", (err, rows, fields) => {
         if(err) throw err;
-        //response
-        //key value pair
-        res.json(rows);
-    });
-});
+        res.json(rows)
 
-//API - REPORT - SEARCH
-//passing the id parameter
-//request - >>> front-end ID
-app.get("/api/members/:id",(req, res)=>{
-    const id=req.params.id; 
-    connection.query(`SELECT * FROM userdata WHERE id='${id}'`, (err, rows, fields)=>{
-        if(err) throw err;
-
-        if(rows.length > 0){
-            res.json(rows);
-        }else{
-            res.status(400).json({msg: `${id} id not found!`})
-        }
     })
-    //res.send(id);
 })
 
+//REPORT - CRUD - SEARCH
+app.get("/api/members/:id", (req, res) => {
+    const id = req.params.id
+    //const first_name = req.params.first_name
+    //res.send(id)
+    //res.send(first_name)
+    connection.query(`SELECT * FROM userdata WHERE id = ${id}`, (err, rows, fields) => {
+        if(err) throw err;
+        if(rows.length > 0){
+            res.json(rows)
+        }else{
+            res.status(400).json({msg:`${id} id not found`})
+        }
+    })
 
-//POST - CREATE
+})
+
+//POST - CREATE - CRUD
 app.use(express.urlencoded({extended: false}))
-app.post("/api/members", (req, res)=>{
+app.post("/api/members", (req, res) => {
     const fname = req.body.fname;
     const lname = req.body.lname;
     const email = req.body.email;
     const gender = req.body.gender;
-    connection.query(`INSERT INTO userdata (first_name, last_name, email, gender) VALUES ('${fname}','${lname}', '${email}', '${gender}')`, (err, rows, fields) =>{
+
+    connection.query(`INSERT INTO userdata (first_name, last_name, email, gender) VALUES ('${fname}', '${lname}', '${email}', '${gender}')`, (err, rows, fields) => {
         if(err) throw err;
-        res.json({msg: `Successfully inserted`});
+        res.json({msg: `Successfully inserted`})
     })
 
 })
 
-//CRUD
-//API
-//PUT - UPDATE
-app.use(express.urlencoded({ extended: false }));
+
+//POST - CREATE - CRUD
+app.use(express.urlencoded({extended: false}))
 app.put("/api/members", (req, res) => {
-  const fname = req.body.fname;
-  const lname = req.body.lname;
-  const email = req.body.email;
-  const gender = req.body.gender;
-  const id = req.body.id;
-  connection.query(
-    `UPDATE userdata SET first_name='${fname}', last_name='${lname}', email='${email}', gender='${gender}' WHERE id='${id}'`,
-    (err, rows, fields) => {
-      if (err) throw err;
-      res.json({ msg: `Successfully updated!` });
-    }
-  );
-});
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+    const gender = req.body.gender;
+    const id = req.body.id;
 
-//DELETE API
-app.use(express.urlencoded({ extended: false }));
-app.delete("/api/members", (req, res) =>{
-    const id=req.body.id;
-    connection.query(`DELETE FROM userdata WHERE id='${id}'`, (err, rows, fields)=>{
-        if(err) throw err
-        res.json({msg: `Successfully deleted!`})
+    connection.query(`UPDATE userdata SET first_name='${fname}', last_name='${lname}', email='${email}', gender='${gender}' WHERE id='${id}'`, (err, rows, fields) => {
+        if(err) throw err;
+        res.json({msg: `Success`})
     })
+
 })
 
+// DELETE
+app.use(express.urlencoded({extended: false}))
+app.delete("/api/members", (req, res) => {
 
+    const id = req.body.id
+    connection.query(`DELETE FROM userdata WHERE id = '${id}'`, (err, rows, fields) => {
+        if(err) throw err;
+        res.json({msg: `Successfully yeeted`})
 
+    })
 
-app.listen(5000, () => {
+})
+
+app.listen(5001, () => {
     console.log(`Server is running in port ${PORT}`);
 })
